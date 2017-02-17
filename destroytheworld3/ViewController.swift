ba487fa5274fbd0b15c05e38584ff7c0cb8ee7dc
Viztor <alvarezfuente@gmail.com>
2017-02-17 19:01:46 +0000

    Initial Commit

---
+++ destroytheworld3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroytheworld3
+//
+//  Created by Victor Manuel Alvarez Fuente on 17/02/2017.
+//  Copyright © 2017 Trovl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
